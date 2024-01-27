fb8724cec599aad7363777f9d160caefb6dc5a96
Max W <j.m.west@wustl.edu>
2024-01-27 12:04:06 -0600

    WOIP

--- blink_with_write.py
+++ blink_with_write.py
@@ -26,9 +26,17 @@ GPIO.setup(LEDPin, GPIO.OUT, initial=GPIO.LOW)
 #Setup for switch
 GPIO.setup(switchPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
+#Command Line Arguments
+flags = ap.ArgumentParser(description="Use the --runtime flag to duration the script will run (default is 60) \n")
+flags.add_argument("--runtime",type = float, default = 30.0, help="Duration the script will run in seconds (float)")
+flags.add_argument("--blinkrate",type = float, default = 1.0, help="Set the blink rate of the LED in seconds (float)")
+flags.add_argument("--debug",type = bool, defualt = False, help="Prints: system time, # iterations,and switch state (boolean)")
+#Dictionary to store command line args
+args = flags.parse_args()
+args = vars(args)
 
 #Controls blink behavior
-def LEDBlink(rate=1.0,pin=LEDPi,f = file):
+def LEDBlink(rate=1.0,pin=LEDPin,f = file):
     GPIO.output(pin,GPIO.HIGH)
     time.sleep(rate)
     GPIO.output(pin,GPIO.LOW)
@@ -76,9 +84,9 @@ def main(runTime):
             
             LEDOff(LEDPin)
         
-        
+    
 
 file = ouput()
-main()
+main(runTime)
 GPIO.cleanup()
 
